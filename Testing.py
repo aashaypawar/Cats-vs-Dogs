f01dad19f81111d8cf25909d32e9d10b8148fde5
Aashay Pawar <41710824+aashaypawar@users.noreply.github.com>
2020-06-27 12:52:37 +0530

    Update Testing.py

--- Testing.py
+++ Testing.py
@@ -2,7 +2,7 @@ from tensorflow import keras
 from keras.preprocessing import image
 import numpy as np
 
-model = keras.models.load_model('output.h5')
+model = keras.models.load_model('new_model.h5')
 
 img_pred = image.load_img('test/12481.jpg',target_size=(150,150))
 img_pred = image.img_to_array(img_pred)
